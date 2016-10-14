21accb726bcd02b8b56fc7f91dbc94f1c1c26b82
Yusuke DOI <doi@preferred.jp>
2016-10-14 18:46:21 +0900

    modified learning process by adding step and offset

--- simple_ae.py
+++ simple_ae.py
@@ -19,24 +19,28 @@ class SimpleAutoEncoder(chainer.Chain):
     "simple autoencoder: input window size: (100,)"
     def __init__(self):
         super(SimpleAutoEncoder, self).__init__(
-            e1=L.Linear(100, 30),
-            e2=L.Linear(30, 4),
-            d1=L.Linear(4, 30),
-            d2=L.Linear(30, 100))
+            e1=L.Linear(100, 40),
+            e2=L.Linear(40, 2),
+            d1=L.Linear(2, 40),
+            d2=L.Linear(40, 100))
         return
-    def __call__(self, x):
+    def encode(self, x):
         h = F.relu(self.e1(x))
         z = F.relu(self.e2(h))
+        return z
+    def decode(self, z):
         h = F.relu(self.d1(z))
         y = self.d2(h)
         return y
+    def __call__(self, x):
+        return self.decode(self.encode(x))
     def loss(self, x):
         y = self(x)
         return F.mean_squared_error(x, y)
 
-def sliding_window(data, steplen=5, window_width=100):
+def sliding_window(data, steplen=5, window_width=100, offset=0):
     "return index and data of sliding window"
-    for i in range(0, len(data) - window_width, steplen):
+    for i in range(offset, len(data) - window_width - offset, steplen):
         yield i, data[i:i+window_width][None]
     return
 
@@ -53,19 +57,25 @@ if __name__ == '__main__':
     opt.use_cleargrads()
     opt.setup(model)
 
-    train_ratio = 0.2
+    train_ratio = 0.1
     window_width = 100
-    steplen=1
+    steplen=3
     epoch=3
 
     train_data = data[:int(len(data)*train_ratio)]
     for e in range(epoch):
-        c = 0
         bar = tqdm(desc="learn({}/{})".format(e, epoch), total=int((len(train_data)-window_width)/steplen), ascii=True)
-        for di, w in sliding_window(train_data, steplen=steplen, window_width=window_width):
+        for di, w in sliding_window(train_data, steplen=steplen, window_width=window_width, offset=e):
             opt.update(model.loss, w) # using bytes
             bar.update()
         bar.close()
+        total_loss = 0
+        c=0
+        for di, w in sliding_window(train_data, steplen=1, window_width=window_width):
+            l = model.loss(w)
+            total_loss += l.data
+            c+=1
+        print('loss={}\n'.format(total_loss/c))
 
     loss = np.zeros((data.shape[0],))
     bar = tqdm(desc="reg.", total=len(data)-window_width, ascii=True)
@@ -74,11 +84,10 @@ if __name__ == '__main__':
         loss[di] = l.data
         bar.update()
     bar.close()
+    print('test_loss={}\n'.format(np.sum(loss)/len(loss)))
 
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
     ax1.plot(data, label="bytes")
     ax2.plot(loss, label="anomaly", color="red", alpha=0.6)
     plt.savefig("loss.png")
-        
-    
